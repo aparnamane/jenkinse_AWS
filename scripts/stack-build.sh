cb900753a0710408ea2cdb211a87f57222294641
Aparna Mane <aparna@Aparnas-MacBook-Pro.local>
2018-10-19 09:39:48 +1100

    Added script and Jenkinsfile

---
+++ scripts/stack-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+$STACKNAME = teststack1
+
+echo "--- Checking if stacks $STACKNAME is exist"
+aws cloudformation describe-stacks --region ap-southeast-2 --stack-name $STACKNAME
+if [ $? == 0 ]; then
+  echo "Updating the $STACKNAME"
+  aws cloudformation update-stack --stack-name $STACKNAME --template-body file://template1.yaml --region ap-southeast-2
+else
+  echo "Creating $STACKNAME"
+  aws cloudformation create-stack --stack-name $STACKNAME --template-body file://template1.yaml --region ap-southeast-2
+fi
